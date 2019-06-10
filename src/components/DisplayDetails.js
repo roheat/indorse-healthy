ae65ecbef379781b10aef4bb75692ccce3391fcf
Rohith Srivathsav <rohithroxtar@gmail.com>
2019-06-10 19:36:46 +0530

    BONUS rank feature added

--- src/components/DisplayDetails.js
+++ src/components/DisplayDetails.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 const DisplayDetails = (props) => {
 
+	const displayRank = () => {
+		let currentRank = 0;
+		const sortedItems = props.items.sort((a,b) => b.calories - a.calories);
+		sortedItems.forEach((item, index) => {
+			if (item.name === props.item.name) {
+				currentRank = index+1;
+			}
+		});
+		return <h2 className="rank">Rank: {currentRank}</h2>
+	}
+
 	return (
 		<div>
 		{
@@ -12,6 +23,7 @@ const DisplayDetails = (props) => {
 				<h3>Ingredients:</h3>
 				<ul>{props.item.ingredients.map((ingredient, i) => <li key={i}>{ingredient}</li>)}</ul>
 				<h3>Calories: {props.item.calories}</h3>
+				{displayRank()}
 			</div>
 			:null
 		}
